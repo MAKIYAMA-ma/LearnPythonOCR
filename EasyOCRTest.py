669dda931461ac83982a62a0765a6b7aa1f3a2aa
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2024-08-17 15:55:46 +0900

    modification to detect texts correctly. But, it needs to improve accuracy and speed.

--- EasyOCRTest.py
+++ EasyOCRTest.py
@@ -1,22 +1,35 @@
+from PIL import Image, ImageDraw, ImageFont
 from matplotlib import pyplot as plt
 import cv2
 import easyocr
+import numpy as np
 import sys
 
 
 def detect_text_easyocr(image_path):
     reader = easyocr.Reader(['ja'])
     img = cv2.imread(image_path)
-    results = reader.readtext(img)
+    prepre_img = pre_process(img)
+    results = reader.readtext(prepre_img)
 
-    for i in range(len(results["text"])):
-        if int(results["conf"][i]) > 60:
-            x, y, w, h = results["left"][i], results["top"][i], results["width"][i], results["height"][i]
-            text = results["text"][i]
+    img_pil = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    draw = ImageDraw.Draw(img_pil)
 
-            print(str(x) + "," + str(y) + "," + str(w) + "," + str(h))
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv2.putText(img, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+    font = ImageFont.truetype("msgothic.ttc", 40)
+
+    for result in results:
+        if result[2] > 0.20:
+            # for tuning up
+            print(result)
+        if result[2] > 0.60:
+            points = np.array(result[0], np.int32)
+            p0 = result[0][0]
+            text = result[1]
+
+            draw.polygon([tuple(points[0]), tuple(points[1]), tuple(points[2]), tuple(points[3])], outline=(0, 0, 255), width=2)
+            draw.text((p0[0], p0[1] - 40), text, font=font, fill=(0, 0, 255))
+
+    img = cv2.cvtColor(np.array(img_pil), cv2.COLOR_RGB2BGR)
 
     plt.imshow(img)
     plt.axis('off')
@@ -28,8 +41,12 @@ def pre_process(img):
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # バイラテラルフィルタでノイズ除去
-    denoised_image = cv2.bilateralFilter(gray_image, 9, 75, 75)
-    return denoised_image
+    # denoised_image = cv2.bilateralFilter(gray_image, 9, 75, 75)
+    # return denoised_image
+
+    # ガウシアンフィルタでノイズ除去
+    blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+    return blurred_image
 
     # # 大津の方法で二値化
     # _, binary_image = cv2.threshold(denoised_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
@@ -42,4 +59,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     image_path = sys.argv[1]
-    detect_text_tesseract(image_path)
+    detect_text_easyocr(image_path)
