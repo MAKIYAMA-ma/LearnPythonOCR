4f7147d7d3f526a2514722bf96f134a03da31085
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2024-08-20 21:11:54 +0900

    modify pre-process to increase accuracy and decrease process time

--- OCRTest.py
+++ OCRTest.py
@@ -13,24 +13,70 @@ class OCRTest(metaclass=abc.ABCMeta):
         pass
 
     def pre_process(self, img):
+        temp_img = img
+
         # グレースケール化
-        gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        if False:
+            temp_img = cv2.cvtColor(temp_img, cv2.COLOR_BGR2GRAY)
+        else:
+            # HSV色空間に変換し、Vチャネルでグレースケール化
+            temp_img = cv2.cvtColor(temp_img, cv2.COLOR_BGR2HSV)
+            temp_img = temp_img[:, :, 2]
 
-        # バイラテラルフィルタでノイズ除去
-        # denoised_image = cv2.bilateralFilter(gray_image, 9, 75, 75)
-        # return denoised_image
+        # ノイズ除去
+        if True:
+            # ガウシアンフィルタでノイズ除去
+            temp_img = cv2.GaussianBlur(temp_img, (5, 5), 0)
+        else:
+            # バイラテラルフィルタでノイズ除去
+            temp_img = cv2.bilateralFilter(temp_img, 9, 75, 75)
 
-        # ガウシアンフィルタでノイズ除去
-        blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
-        return blurred_image
+        # 大津の方法で二値化
+        # _, temp_img = cv2.threshold(temp_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-        # # 大津の方法で二値化
-        # _, binary_image = cv2.threshold(denoised_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        # 傾き補正
+        # coords = np.column_stack(np.where(temp_img > 0))
+        # angle = cv2.minAreaRect(coords)[-1]
+        # if angle < -45:
+        #     angle = -(90 + angle)
+        # else:
+        #     angle = -angle
+        # (h, w) = temp_img.shape[:2]
+        # center = (w // 2, h // 2)
+        # M = cv2.getRotationMatrix2D(center, angle, 1.0)
+        # temp_img = cv2.warpAffine(temp_img, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
 
-        # return binary_image
+        # コントラスト調整
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+        temp_img = clahe.apply(temp_img)
+
+        if False:
+            # for Debug
+            # 前処理後の画像確認
+            plt.imshow(temp_img, cmap='gray')
+            plt.axis('off')
+            plt.show()
+
+        return temp_img
+
+    def resize_image(self, image, target_size=1000):
+        # 短辺をtarget_sizeとする倍率を計算
+        height, width = image.shape[:2]
+        if height < width:
+            scale = target_size / height
+        else:
+            scale = target_size / width
+
+        # 画像のリサイズ
+        new_width = int(width * scale)
+        new_height = int(height * scale)
+        resized_image = cv2.resize(image, (new_width, new_height))
+
+        return resized_image
 
     def run(self, image_path: str):
         img = cv2.imread(image_path)
+        img = self.resize_image(img)
         prepro_img = self.pre_process(img)
         img = self.detect_text_ocr(prepro_img, img)
 
